cee24eb9791ceb9b294b9606239543d5936ab83a
“[name]” <g5ada02@hackademy.mx>
2021-06-01 03:33:56 -0500

    commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,24 @@
 const chatForm = document.getElementById('chat-form')
+// Constante que toma el estilo del elemento de mi document
+const chatMessage = document.querySelector('.chat-messages')
+
+// Obteniendo username y room del URL
+const {username,room} = Qs.parse(location.search,{
+  ignoreQueryPrefix: true
+});
+
 const socket = io();
+
+//Unirse a la sala
+socket.emit('joinRoom', {username, room});
 
 socket.on('message', message =>{
   //console.log(message);
   outputMessage(message);
 
-})
+  //Top del scroll
+  chatMessage.scrollTop = chatMessage.scrollHeight;
+});
 
 chatForm.addEventListener('submit',(e)=>{
   e.preventDefault();
@@ -15,15 +28,19 @@ chatForm.addEventListener('submit',(e)=>{
 
   socket.emit('chatMessage', msg)
 
+  //limpiar input y mantener el focus en el input
+  e.target.elements.msg.value ='';
+  e.target.elements.msg.focus();
+
   //console.log(msg)
 })
 
-function outputMessage(msg){
+function outputMessage(message){
   const div = document.createElement('div');
   div.classList.add('message');
-  div.innerHTML= `<p class= = "meta">  <span>9:12pm</span></p>
+  div.innerHTML= `<p class= = "meta"> ${message.username} <span>${message.time}</span></p>
   <p class= "text">
-    ${msg}
+    ${message.text}
   </p>`
 
   document.querySelector('.chat-messages').appendChild(div);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require ('http');
 const express = require('express');
 const socketio = require ('socket.io')
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,20 +10,22 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname,'public')));
 
+const botName= 'ChatBot';
+
 io.on('connection', socket =>{
 
     //Mensaje solo al usuario que se conecta
-    socket.emit('message','Welcome to chat');
+    socket.emit('message',formatMessage( botName, 'Welcome to chat'));
     
     //Mensaje a todos los clientes o usuario, excepto el que se esta conectando en el momento
-    socket.broadcast.emit('message', 'Un usuario se ha conectado al chat');
+    socket.broadcast.emit('message', formatMessage(botName,'Un usuario se ha conectado al chat'));
 
 
     socket.on('chatMessage', msg =>{
         console.log(msg);
         
         //Meensaje a todos los usuarios conectados en el socket
-        io.emit('message', msg);
+        io.emit('message', formatMessage('User', msg));
     });
 
 });
